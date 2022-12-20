02f3acc8811a77e9d5dcb3acd2b937a86eed1487
David Prakash <118829009+davidprakash12@users.noreply.github.com>
2022-12-20 16:22:37 +0530

    Add files via upload

---
+++ funwithdefaultvalue.js
@@ -0,0 +1,16 @@
+<html lang="en">
+<head>
+    <script>
+        function show(id=0,name="no name"){
+            document.write("Your name is <b>"+name+"</b> and your id is <b>"+id+"</b><br /><br />");
+        }
+    </script>
+</head>
+<body>
+    <script>
+        show(12,"pankaj");
+        show(12);
+        show();
+    </script>
+</body>
+</html>
